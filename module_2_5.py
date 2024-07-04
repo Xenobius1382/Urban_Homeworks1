54027218d16f8d479046f67612dc77d3f342bb3e
Max <shubinmax13@gmail.com>
2024-07-04 21:26:12 +0300

    test

--- module_2_5.py
+++ module_2_5.py
@@ -1,16 +1,16 @@
 # закрепление навыков написания функций и их вызовов
 
 
-def get_matrix(n, m, value):
-    matrix = []
+def get_matrix(n, m, value):          # создаем функцию которая будет заполнять матрицу
+    matrix = []                       # создаем пустой список
     for i in range(n):
-        matrix.append([])
+        matrix.append([])             # заполняем созданный список пустыми списками формирующими матрицу в количестве n
         for k in range(m):
-            matrix[i].append(value)
+            matrix[i].append(value)   # заполняем добавленные списки значениями value в количестве m
     return matrix
-result_1 = get_matrix(8, 5, 6)
+result_1 = get_matrix(8, 5, 6)  # вызываем функцию и передаем ей значения
 result_2 = get_matrix(3, 7, 2)
 result_3 = get_matrix(5, 9, 23)
-print(result_1)
+print(result_1)                              # выводим результат в консоль
 print(result_2)
 print(result_3)
