499d4170640d3ef2357ae6ab8933cc8a0ac0d70b
Max <shubinmax13@gmail.com>
2024-10-01 21:22:09 +0300

    test

--- module_8_1.py
+++ module_8_1.py
@@ -5,14 +5,14 @@
 
 def add_everything_up(a, b):
     try:
-        return print(a + b)
+        return a + b
     except TypeError:
         c = str(a) + str(b)
-        print(c)
+        return c
 
 
-add_everything_up(123.456, 'строка')
-add_everything_up('яблоко', 4215)
-add_everything_up(123.456, 7)
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
 
 
